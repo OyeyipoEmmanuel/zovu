b821c888b42d10b479be4925b1ab4bb2c6d52b8e
Emmanuel <emmasco0207@gmail.com>
2026-05-11 00:03:42 +0100

    feat: Landing page and auth screens

---
+++ frontend/src/features/Auth/screens/index.ts
@@ -0,0 +1,6 @@
+export { PersonalInfo } from './PersonalInfo';
+export { RoleInfo } from './RoleInfo';
+export { IdentityVerification } from './IdentityVerification';
+export { FinancialProfile } from './FinancialProfile';
+export { SignupSuccess } from './SignupSuccess';
+export { Login } from './Login';
